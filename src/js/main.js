4e8444cf2fa1b5e411d2a57f4817053af3705015
Cong Min <i@congm.in>
2016-10-15 17:24:14 +0800

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@
  * @ Cong Min
  */
 require('../sass/style.scss');
+/* CNZZ统计 */
+(function() {
+    var box = document.createElement("div"),
+        cnzz = document.createElement("script");
+    box.style.display = "none";
+    cnzz.src = "//s1.cnzz.com/z_stat.php?id=1260605208";
+    box.appendChild(cnzz);
+    document.body.appendChild(box);
+})();
