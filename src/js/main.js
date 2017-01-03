46e8b99e6a253505368c2e42f147c82a2ad429af
Cong Min <i@congm.in>
2017-01-03 21:33:48 +0800

    公测

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,46 @@
  * @ Cong Min
  */
 require('../sass/style.scss');
+var $ = jQuery = require('jquery');
+require('typed.js');
+$(function(){
+    $(".define-text").typed({
+        strings: ["微信小程序", "校园移动门户", "搜人利器", "点名神器", "掌上课程表", "考试倒计时", "校园生活助手", "百事通", "期待您的发现"],
+        typeSpeed: 100,
+        backDelay: 5000,
+        backSpeed: 50,
+        loop: true,
+        cursorChar: "_",
+        preStringTyped: function() {
+            var list = $('.docs-text'),
+                active = $('.docs-text.active'),
+                next;
+            if(!active.length){
+                next = $(list[0]);
+            }else{
+                next = active.next();
+                if (!next.length) {
+                    next = $(list[0]);
+                }
+                active.removeClass('active');
+            }
+            next.addClass('active');
+            var box_inner = $('.box-inner'),
+                left = box_inner.data('left');
+            if(!left || left >= 235*9) { left = 0; }
+            left = left + 235;
+            box_inner.css('transform', 'translate3d(-'+left+'px,0,0)');
+            box_inner.data('left', left);
+        }
+    });
+});
+
 /* CNZZ统计 */
-(function() {
+$(function() {
     var box = document.createElement("div"),
         cnzz = document.createElement("script");
     box.style.display = "none";
     cnzz.src = "//s1.cnzz.com/z_stat.php?id=1260605208";
     box.appendChild(cnzz);
     document.body.appendChild(box);
-})();
+});
